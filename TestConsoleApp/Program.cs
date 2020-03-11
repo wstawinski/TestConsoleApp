9aa0936c69f8efcc8720ce979ad2410969d97e67
Wojciech Stawiński <Stawinski@vialutions.com>
2020-03-11 19:08:54 +0100

    Initial commit

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new decimal();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(d);
+        }
+    }
+}
